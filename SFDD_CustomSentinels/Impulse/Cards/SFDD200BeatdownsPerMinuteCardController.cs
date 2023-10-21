e9098f09cec7ecd52008aa4916b88aedd9f531d9
Mint Gould <wispymouse@gmail.com>
2023-10-21 16:58:01 -0700

    Created blank card controller for each of the cards

---
+++ SFDD_CustomSentinels/Impulse/Cards/SFDD200BeatdownsPerMinuteCardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System.Collections;
+
+namespace SFDD_CustomSentinels.SFDDImpulse
+{
+    public class SFDD200BeatdownsPerMinuteCardController : CardController
+    {
+        public SFDD200BeatdownsPerMinuteCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+        }
+
+        public override IEnumerator Play()
+        {
+            return this.GameController.SendMessageAction("You knew this card does nothing. Why would you play it?", Priority.Medium, GetCardSource());
+        }
+    }
+}
---
+++ SFDD_CustomSentinels/Impulse/Cards/SFDDPhaseModulatorCardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System.Collections;
+
+namespace SFDD_CustomSentinels.SFDDImpulse
+{
+    public class SFDDPhaseModulatorCardController : CardController
+    {
+        public SFDDPhaseModulatorCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+        }
+
+        public override IEnumerator Play()
+        {
+            return this.GameController.SendMessageAction("You knew this card does nothing. Why would you play it?", Priority.Medium, GetCardSource());
+        }
+    }
+}
